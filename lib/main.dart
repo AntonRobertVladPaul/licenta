53da8044b88a82d7bb8d833469a7fb43defa6795
Vlad <vvladester@gmail.com>
2023-06-03 13:05:24 +0300

    Useless vars removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:licenta/bootstrap.dart';
 import 'package:licenta/core/core.dart';
-import 'package:licenta/firebase_options_dev.dart';
 
 Future<void> main() async {
-  final defaultOptions = DefaultFirebaseOptions.currentPlatform;
   await bootstrap(
     networkConstants: DevConstants(),
   );
